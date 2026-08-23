f14f33b16a6ef8f6e323764dbbdb939e204e042e
1e1001 <michael@malinov.com>
2026-08-23 11:47:30 -0700

    rename entrances to their trigger

--- donutcounty/regions.py
+++ donutcounty/regions.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from BaseClasses import Region
-from rule_builder.rules import True_
 
 from .options import GoalArea
 
@@ -16,7 +15,7 @@ def create_and_connect_regions(world: DonutCountyWorld) -> None:
     def world_region(name, parent, rules):
         region = Region(name, world.player, world.multiworld)
         world.multiworld.regions.append(region)
-        world.get_region(parent).connect(region, "Enter " + name, rules)
+        world.get_region(parent).connect(region, ("Start " + name) if parent == "Menu" else ("Complete " + parent), rules)
     menu = Region("Menu", world.player, world.multiworld)
     aftermath = Region("Aftermath0", world.player, world.multiworld)
     texting = Region("Texting", world.player, world.multiworld)
@@ -27,7 +26,7 @@ def create_and_connect_regions(world: DonutCountyWorld) -> None:
     world.get_region("ChickenBarn1").connect(texting, "Texting ChickenBarn")
     world.get_region("RaccoonHQ0").connect(texting, "Texting RaccoonHQ")
     aftermath_parent = world.get_region("BossFight3") if world.options.goal_area == GoalArea.option_bossfight else menu
-    aftermath_parent.connect(aftermath, "Enter Aftermath0")
+    aftermath_parent.connect(aftermath, "Start Aftermath0")
 
     
     
